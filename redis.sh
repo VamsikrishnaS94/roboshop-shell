14818689510f21acd69be54b2f37dcd8ec7a8130
krishna.svk <krishna.svk94@gmail.com>
2023-07-19 22:05:04 +0530

    /home/centos/roboshop-shell

--- redis.sh
+++ redis.sh
@@ -8,7 +8,7 @@ echo -e "\e[33m install redis \e[0m"
 yum install redis -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m update redis listen address \e[0m"
-sed -i 's/127.0.01/0.0.0.0/'/etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
+sed -i 's/127.0.0.1/0.0.0.0/'/etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
 
 echo -e "\e[33m start redis service \e[0m"
 systemctl enable redis &>>/tmp/roboshop.log
