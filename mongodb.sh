7497eeb6f360cdb60e9ed2023ef2af6d94d7ff39
krishna.svk <krishna.svk94@gmail.com>
2023-07-19 19:13:05 +0530

    Frontend script
    rm -rf /app
    before cxreating app directory we remove content as we re-run the shell script multiple times with some modifications.

--- mongodb.sh
+++ mongodb.sh
@@ -1,12 +1,12 @@
-echo -e "\e[33mCopy mongodb repo file\e[0m"
+echo -e "\e[33m Copy mongodb repo file\e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongo.repo  &>>/tmp/roboshop.log
 
-echo -e "\e[33minstalling mongodb\e[0m"
+echo -e "\e[33m installing mongodb\e[0m"
 yum install mongodb-org -y  &>>/tmp/roboshop.log
 
-echo -e "\e[33mupdate mongodb listen address\e[0m"
+echo -e "\e[33m update mongodb listen address\e[0m"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
-echo -e "\e[33mstart mongodb service\e[0m"
+echo -e "\e[33m start mongodb service\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod &>>/tmp/roboshop.log
