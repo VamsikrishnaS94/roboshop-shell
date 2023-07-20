9e1c366bf29a85764f61ea5136c6f4538ac4199d
krishna.svk <krishna.svk94@gmail.com>
2023-07-20 23:06:38 +0530

    mysql script

---
+++ shipping.sh
@@ -0,0 +1,32 @@
+echo -e "\e[33m install maven \e[0m"
+yum install maven -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m add user roboshop \e[0m"
+useradd roboshop &>>/tmp/roboshop.log
+
+echo -e "\e[33m make app directory  \e[0m"
+rm -rf /app &>>/tmp/roboshop.log
+mkdir /app
+
+echo -e "\e[33m download application content  \e[0m"
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
+
+echo -e "\e[33m extract application content \e[0m"
+unzip /tmp/shipping.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
+
+echo -e "\e[33m download maven dependencies  \e[0m"
+mvn clean package &>>/tmp/roboshop.log
+mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
+
+echo -e "\e[33m install mysql client  \e[0m"
+yum install mysql -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m load schema \e[0m"
+mysql -h mysql-dev.vamsidevops.cloud -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/roboshop.log
+
+echo -e "\e[33m start shipping service  \e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable shipping &>>/tmp/roboshop.log
+systemctl restart shipping &>>/tmp/roboshop.log
